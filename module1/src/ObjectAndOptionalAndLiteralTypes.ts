0e9402e7a60c945e5ce27f591f50c632d5b1ae6b
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-30 22:45:27 +0600

    Understandinof Object, Optional and Literal Types of Typescript

---
+++ module1/src/ObjectAndOptionalAndLiteralTypes.ts
@@ -0,0 +1,66 @@
+// Ts in Object 
+
+// raw
+ const user = {
+    firstName : 'Shahnawaz',
+    middleName :"s",
+    lastName : "Azid"
+ }
+
+
+// with ts
+
+const user1: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+} = {
+    firstName: 'Shahnawaz',
+    middleName: "s",
+    lastName: "Azid"
+}
+
+
+// Optional types
+
+const user2: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+} = {
+    firstName: 'Shahnawaz',
+    lastName: "Sazid"
+}
+
+
+// Literal types
+
+const user3: {
+    company : "Programming Hero"
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+} = {
+    company: "Programming Hero",
+    firstName: 'Shahnawaz',
+    lastName: "Sazid"
+}
+
+// user3.company = "PH"
+// eta korle error dibe since its not assignable
+
+// Another way of making Literal types
+
+const user4: {
+    readonly company : string
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+} = {
+    company: "Programming Hero",
+    firstName: 'Shahnawaz',
+    lastName: "Sazid"
+}
+
+// user4.company = "PH"
+// eta korle error dibe since its not assignable
