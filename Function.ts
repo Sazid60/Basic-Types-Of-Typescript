64d4954544cd80f0b34d30dcc059184003ed4490
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-10-30 19:27:44 +0600

    Understaning the Typescript in Function

---
+++ Function.ts
@@ -0,0 +1,33 @@
+//Learning Functions in Typescript
+
+//  Normal Function
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+add(2, 2)
+
+// Arrow Function 
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+//  Default Value setting
+function add2(num1: number, num2: number = 10): number {
+    return num1 + num2;
+}
+add(2, 10)
+
+
+// Method (kuno object er moddhe jodi function thake tokhon tahke method bole)
+
+const poorUser = {
+    name: "Sazid",
+    balance: 0,
+    addBalance(balance: number): string {
+        return `My New Balance : ${this.balance + balance}`
+    }
+}
+
+// complex playing with array
+
+const array : number[] = [1,2,3,4,5]
+
+const newArray : number [] = array.map(((element : number) : number=> element*element))
