c290008ca1027a332bc1c97e8ae9244bf4e17f6f
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-11-02 12:10:20 +0600

    Tying Json Placeholder

--- module2/src/2.8.AsyncronusTypescript.ts
+++ module2/src/2.8.AsyncronusTypescript.ts
@@ -43,5 +43,21 @@
         return data
     }
 
-    
+    // fetching from json place holder
+
+    type Todo = {
+        id:number;
+        userId :number;
+        title:string;
+        completed : boolean;
+    }
+    const getTodo = async(): Promise<Todo> =>{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+        const data = await response.json()
+
+        console.log(data)
+        return data
+    }
+    getTodo()
 }
